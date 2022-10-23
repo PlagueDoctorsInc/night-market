1d5e4294fda28193427b9427b413fa3ee2889763
PlageDoctorsInc <jenkpase@gmail.com>
2022-10-23 13:04:12 -0400

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import { Web3Modal } from '@web3modal/react'
+
+const modalConfig = {
+  projectId: '5d505c3da78b67e87d25921985997ce2',
+  theme: 'dark',
+  accentColor: 'blue',
+  ethereum: {
+    appName: 'web3Modal',
+  },
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Web3Modal config={modalConfig} />
+    </>
+  )
 }
 
 export default MyApp
